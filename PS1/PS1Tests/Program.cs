c8cc70a49bd70ba236c2cc07310102e95c42d44e
jk2112 <jarem.k.kilby@gmail.com>
2016-09-05 15:48:16 -0600

    Made rough draft of box 1 of algorithm

---
+++ PS1/PS1Tests/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PS1Tests
+{
+    //TODO make this inherit other project
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FormulaEvaluator.Evaluator.Evaluate("5 5 10");
+        }
+    }
+}
